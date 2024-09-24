787c65571964c2d4e144e162140fc6f7dcb2f36d
1Dohyeon <dh1072005@gmail.com>
2024-09-24 20:18:50 +0900

    add : project 페이지 추가

---
+++ projects/project.js
@@ -0,0 +1,18 @@
+const items = document.querySelectorAll(".project-section1-content1-item");
+
+items.forEach((item) => {
+  const subList = item.querySelector(".project-section1-content1-sub-list");
+
+  item.addEventListener("click", () => {
+    item.classList.toggle("open"); // 'open' 클래스를 토글하여 서브리스트 열기/닫기
+
+    if (item.classList.contains("open")) {
+      // 서브리스트의 실제 높이를 계산하여 max-height에 동적으로 설정
+      const subListHeight = subList.scrollHeight;
+      subList.style.maxHeight = subListHeight + "px";
+    } else {
+      // 닫힐 때 max-height를 0으로 설정
+      subList.style.maxHeight = "0";
+    }
+  });
+});
