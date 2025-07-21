739e8d0d7ace7697d2548dbb872c271dd6813a69
Dohyeon Won <dh1072005@gmail.com>
2025-07-21 19:54:44 +0900

    [UPDATE] what i study study card border 수정

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -4,7 +4,7 @@ import "../../styles/Footer.css";
 const Footer = () => {
     return (
         <footer>
-            <p>ⓒ 2025 1Dohyeon's page.</p>
+            <p>ⓒ 2025 1Dohyeon's Portfolio.</p>
         </footer>
     );
 };
